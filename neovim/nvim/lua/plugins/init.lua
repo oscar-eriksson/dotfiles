113330ab9f26ff4731a873de916195f1fb26d6c0
oscar-eriksson <oscar_eriksson_96@hotmail.com>
2024-04-24 17:28:02 +0200

    WIP nvchad config

--- neovim/nvim/lua/plugins/init.lua
+++ neovim/nvim/lua/plugins/init.lua
@@ -8,23 +8,40 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+      require("custom.configs.lspconfig")
+    end,
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end
+  }
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server",
+        "stylua",
+  			"html-lsp",
+        "css-lsp" ,
+        "prettier",
+        "typescript-language-server",
+        "eslint-lsp"
+  		},
+  	},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim"
+  }
   --
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
