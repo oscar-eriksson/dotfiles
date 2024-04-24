7d2bcbb819adb5dc89c656ade8c1d795e6f591df
oscar-eriksson <oscar_eriksson_96@hotmail.com>
2024-04-24 11:14:14 +0200

    Changed ohmyzsh

--- install/install-deps-apt.sh
+++ install/install-deps-apt.sh
@@ -7,18 +7,24 @@ sudo apt-get install zsh -y
 # set default shell
 sudo chsh -s /usr/bin/zsh
 
+
+# extra deps
+sudo apt-get install fzf tldr curl -y
+
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --keep-zshrc --skip-chsh"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 # TODO Install docker ?
 
 # TODO Install tmux with WSL
 
 # TODO Install nvim
-
-# extra deps
-sudo apt-get install fzf tldr -y
